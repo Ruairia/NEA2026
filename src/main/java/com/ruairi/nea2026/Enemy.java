46b40f03d8e22b2049ffe0cc44ed4c7b31dc5e28
ruairi <ruairianthony@outlook.com>
2025-09-10 20:55:52 +0100

    More class work

--- src/main/java/com/ruairi/nea2026/Enemy.java
+++ src/main/java/com/ruairi/nea2026/Enemy.java
@@ -1,7 +1,20 @@
-package com.ruairi.nea2026;
+    package com.ruairi.nea2026;
 
-public abstract class Enemy extends Sprite{
-    public Enemy(float posX, float posY, float velocityX, float velocityY, float width, float height) {
-        super(posX, posY, velocityX, velocityY, width, height);
+    public abstract class Enemy extends Sprite implements EnemyControls{
+        public Enemy(float posX, float posY, float velocityX, float velocityY, float width, float height) {
+            super(posX, posY, velocityX, velocityY, width, height);
+        }
+
+
+        public void goToPoint(float moveSpeed, float pointX, float pointY){
+            float deltaX = pointX - posX;
+            float deltaY = pointY - posY;
+            float deltaDist = (float) Math.sqrt(deltaX*deltaX + deltaY*deltaY);
+            velocityX = moveSpeed*deltaX/deltaDist;
+            velocityY = moveSpeed*deltaY/deltaDist;
+        }
+        public void goToPoint(float moveSpeed, float pointX){
+            goToPoint(moveSpeed, pointX, posY);
+        }
+
     }
-}
---
+++ src/main/java/com/ruairi/nea2026/EnemyControls.java
@@ -0,0 +1,6 @@
+package com.ruairi.nea2026;
+
+public interface EnemyControls{
+    public static void goToPoint(float moveSpeed, float pointX, float pointY) {
+    }
+}
